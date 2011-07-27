23407ec7c0051044f8066b32b96cca2f3e5fba25
Alexey Churkin <phoebus@osiacat.ru>
2011-07-27 14:49:55 +0300

    File control implemented

--- lib/Web/Form/controls/FileFormControl.class.php
+++ lib/Web/Form/controls/FileFormControl.class.php
@@ -22,27 +22,151 @@
  */
 class FileFormControl extends InputFormControl
 {
-	const ERROR_UPLOAD_FAILED = 'upload failed';
-	const ERROR_FILETYPE = 'unexpected file type';
-	const ERROR_FILESIZE = 'unexpected file size';
+	// missing
+	const ERROR_NOTHING = 'nothing was uploaded'; // UPLOAD_ERR_NO_FILE
+
+	// wrong
+	const ERROR_SIZE = 'file exceeds the allowed size'; //UPLOAD_ERR_INI_SIZE, UPLOAD_ERR_FORM_SIZE,
+	const ERROR_PARTIAL = 'file was partially uploaded'; // UPLOAD_ERR_PARTIAL
+	const ERROR_DENIED = 'file of that type is not allowed'; // UPLOAD_ERR_EXTENSION
+
+	// asserts: UPLOAD_ERR_NO_TMP_DIR, UPLOAD_ERR_CANT_WRITE
+
+	private $filepath;
+	private $maxSize;
+	private $noEmpty = true;
+
+	function setMaxSize($bytes)
+	{
+		Assert::isPositiveInteger($bytes);
+
+		$this->maxSize = $bytes;
+
+		return $this;
+	}
+
+	function setNoEmptyFile($flag = true)
+	{
+		Assert::isBoolean($flag);
+
+		$this->noEmpty = $flag;
+
+		return $this;
+	}
+
+	function move($filepath)
+	{
+		return move_uploaded_file($this->getImportedFilepath(), $filepath);
+	}
+
+	function getImportedName()
+	{
+		$value = $this->getValue();
+		if ($value) {
+			return $value['name'];
+		}
+	}
+
+	function getImportedFilepath()
+	{
+		$value = $this->getValue();
+		if ($value) {
+			return $value['tmp_name'];
+		}
+	}
+
+	function getImportedSize()
+	{
+		$value = $this->getValue();
+		if ($value) {
+			return $value['size'];
+		}
+	}
 
 	function getType()
 	{
 		return 'file';
 	}
 
+	final function setDefaultValue($value)
+	{
+		// do nothing
+	}
+
 	function importValue($value)
 	{
-		Assert::notImplemented();
+		if (
+				!$value
+				|| !is_array($value)
+				|| !isset($value['name'])
+				|| !isset($value['tmp_name'])
+				|| !isset($value['error'])
+				|| !isset($value['size'])
+		) {
+			$this->markMissing(self::ERROR_NOTHING);
+			return false;
+		}
+		else if ($value['error']) {
+			switch ($value['error']) {
 
-		if (!isset($value['tmp_name']) || !is_uploaded_file($value['tmp_name'])) {
-			$this->addError(self::ERROR_UPLOAD_FAILED, 'file is not uploaded during network lags');
-			return;
+				case UPLOAD_ERR_NO_FILE: {
+					$this->markMissing(self::ERROR_NOTHING);
+					break;
+				}
+
+				case UPLOAD_ERR_INI_SIZE:
+				case UPLOAD_ERR_FORM_SIZE: {
+					$this->markWrong(self::ERROR_SIZE);
+					break;
+				}
+
+				case UPLOAD_ERR_PARTIAL: {
+					$this->markWrong(self::ERROR_PARTIAL);
+					break;
+				}
+
+				case UPLOAD_ERR_EXTENSION: {
+					$this->markWrong(self::ERROR_DENIED);
+					break;
+				}
+
+				case UPLOAD_ERR_NO_TMP_DIR:
+				case UPLOAD_ERR_CANT_WRITE: {
+					Assert::isUnreachable('Unable to save tmp file');
+					break;
+				}
+			}
+
+			return false;
+		}
+		else if (!is_uploaded_file($value['tmp_name'])) {
+			$this->markMissing(self::ERROR_NOTHING);
+			return false;
 		}
 
-		Assert::notImplemented();
+		// perform extra checks
+		if ($this->noEmpty && !$value['size']) {
+			$this->markMissing(self::ERROR_NOTHING);
+			return false;
+		}
 
-		return $this->reset()->setValue($value);
+		if ($this->maxSize && $this->maxSize < $value['size']) {
+			$this->markWrong(self::ERROR_SIZE);
+			return false;
+		}
+
+		return parent::importValue($value);
+	}
+
+	function __destruct()
+	{
+		$value = $this->getValue();
+		if ($value) {
+			try {
+				unlink($value);
+			}
+			catch (ExecutionContextException $e) {}
+		}
 	}
 
 	function toHtml(array $htmlAttributes = array())
--- lib/Web/Form/controls/FileFormControlSet.class.php
+++ lib/Web/Form/controls/FileFormControlSet.class.php
@@ -29,6 +29,33 @@ class FileFormControlSet extends FormControlSet
 		$this->setDefaultValue(array_fill(0, $defaultInputCount, $this->getLabel()));
 	}
 
+	function importValue($value)
+	{
+		if (
+				$value
+				&& is_array($value)
+				&& isset($value['name'])
+				&& isset($value['tmp_name'])
+				&& isset($value['error'])
+				&& isset($value['size'])
+		) {
+			$fixed = array();
+			foreach ($value['tmp_name'] as $idx => $_) {
+				$fixed[$idx] = array(
+					'name'     => $value['name'][$idx],
+					'tmp_name' => $value['tmp_name'][$idx],
+					'size'     => $value['size'][$idx],
+					'error'    => $value['error'][$idx],
+					'type'     => (isset($value['type'][$idx])? $value['type'][$idx]: null),
+				);
+			}
+
+			$value = $fixed;
+		}
+
+		return parent::importValue($value);
+	}
+
 	protected function spawnSingle()
 	{
 		return new FileFormControl($this->getInnerName(), $this->getLabel());
