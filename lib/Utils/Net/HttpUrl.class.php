e1c15a4e5a3d776488b57acfd9eac91484b1f35d
Alexey Churkin <phoebus@osiacat.ru>
2018-02-23 13:44:17 +0300

    Fix

--- lib/Utils/Net/HttpUrl.class.php
+++ lib/Utils/Net/HttpUrl.class.php
@@ -322,19 +322,9 @@ class HttpUrl implements IStringCastable
 		$out[] = $this->getHost();
 
 		// Port
-		if ($this->getHost() && !in_array($this->getPort(), [DEFAULT_HTTP_PORT, DEFAULT_HTTPS_PORT])) {
-			// if (
-			// 		   !(
-			// 		   		   $this->getScheme() == 'http'
-			// 		   		&& $this->getPort() == self::DEFAULT_HTTP_PORT
-			// 		)
-			// 		&& !(
-			// 				   $this->getScheme() == 'https'
-			// 				&& $this->getPort() == self::DEFAULT_HTTPS_PORT
-			// 		)
-			// ) {
+		$port = $this->getPort();
+		if ($port && $port != DEFAULT_HTTP_PORT && $port != $DEFAULT_HTTPS_PORT) {
 				$out[] = ':' . $this->getPort();
-			// }
 		}
 
 		$out[] = $this->encodePath($this->getPath());
